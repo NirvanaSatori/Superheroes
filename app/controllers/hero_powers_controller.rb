f4ec74462690d5a6ef9f98c8b0438616b78a1384
MORRIS <morris24kariuki@gmail.com>
2023-03-28 19:43:02 +0300

    added a hero_params private method

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,11 +1,17 @@
 class HeroPowersController < ApplicationController
 
     def create
-        heropower = HeroPower.create(strength: params[:strength], hero_id: params[:hero_id], power_id: params[:power_id])
+        heropower = HeroPower.create(hero_params)
         if heropower
             render json: hero, include: :powers, status: :created
         else 
             render json: {"errors": ["validation errors"]}
         end
+    end
+
+    private 
+
+    def hero_params
+        params.permit(strength: params[:strength], hero_id: params[:hero_id], power_id: params[:power_id])
     end
 end
