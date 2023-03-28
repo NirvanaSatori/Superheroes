6641ff7069b1f32456bb54bb0ca384acc5477b61
MORRIS <morris24kariuki@gmail.com>
2023-03-28 20:57:03 +0300

    added seeding data

--- app/models/power.rb
+++ app/models/power.rb
@@ -3,5 +3,5 @@ class Power < ApplicationRecord
     has_many :hero_powers
     has_many :heros, through: :hero_powers
 
-    validations :description, length: {minimum: 20}
+    validates :description, length: {minimum: 20}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+print "start seeding"
+Power.create(name:"speed", description:"superspeed")
+Hero.create(name:"Doe", super_name:"SuperDoe")
+HeroPower.create(strength:"Strong",power_id:1, hero_id:1)
+
+print "done seeding"
